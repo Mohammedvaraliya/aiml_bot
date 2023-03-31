90aea930b9b645460db9a9f70de80b03649022a4
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-03-31 17:20:33 +0530

    Add deployment files

--- aiml_bot/settings.py
+++ aiml_bot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cj=ht-%uxm-eoya446t2ynvd5p#0w-t84iix4i$-4s=a=gb=_h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
