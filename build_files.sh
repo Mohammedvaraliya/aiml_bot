c0a3297f3c5698803b2742ac4c90cc8f5000d81c
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-03-31 16:40:17 +0530

    Add deployment files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END"
