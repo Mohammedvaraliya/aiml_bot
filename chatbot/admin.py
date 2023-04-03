1809608bde81515909fbe4e5ba3a2bd06579872c
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-04-03 12:51:54 +0530

    Added questions

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Category
 
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ['id', 'text']
 
 admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Category)
--- chatbot/models.py
+++ chatbot/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Question(models.Model):
     text = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__(self) -> str:
         return self.text
